88bd2e4eeefcf5826e7fa26a9dc28c70d8d09ad2
Abdullah-Zihad-100 <zihadahmad60@gmail.com>
2026-07-10 12:58:14 +0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AdminProvider } from "./context/AdminContext";
+import { AdminProvider } from "./context/AdminContext.jsx";
 import { Toaster } from "react-hot-toast";
 import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
