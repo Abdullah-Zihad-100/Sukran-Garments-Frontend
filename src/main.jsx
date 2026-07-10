4f5461c080bde0e46bf58b184250c826bb803d2b
Abdullah-Zihad-100 <zihadahmad60@gmail.com>
2026-07-10 20:28:32 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AdminProvider } from "./context/AdminContext.js";
+import { AdminProvider } from "./context/AdminContext.jsx";
 import { Toaster } from "react-hot-toast";
-import { FavoritesProvider } from "./context/FavoritesContext.js";
+import { FavoritesProvider } from "./context/FavoritesContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
